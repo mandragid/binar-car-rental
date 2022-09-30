7573cf0c84247f42efed2bb0274666c1cb02bb74
Andre <andreivanp@gmail.com>
2022-09-30 19:35:27 +0700

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,37 +9,44 @@ import Button from "react-bootstrap/Button";
 import GambarMobil from "../img/mercy.png";
 
 function NavBar() {
-	return (
-		<div class="container">
-			<div className="NavBar">
-				<Navbar expand="lg d-flex">
-					<Container className="container d-flex justify-content-end float-end">
-						<Navbar.Brand href="#home">BCR</Navbar.Brand>
-						<Navbar.Toggle aria-controls="basic-navbar-nav" />
-						<Navbar.Collapse id="justify-content-end float-end">
-							<Nav className="justify-content-end float-end">
-								<Nav.Link href="#home">Our Services</Nav.Link>
-								<Nav.Link href="#link">Why Us</Nav.Link>
-								<Nav.Link href="#link">Testimonial</Nav.Link>
-								<Nav.Link href="#link">FAQ</Nav.Link>
-							</Nav>
-						</Navbar.Collapse>
-					</Container>
-				</Navbar>
-
-				<div class="row">
-					<div class="col">column</div>
-					<div class="col gambar">gambar</div>
-				</div>
-				<div class="row">
-					<div class="col">column</div>
-				</div>
-				<div class="row">
-					<div class="col">column</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div class="Hero d-flex">
+      <div className="NavBar">
+        <Navbar expand="lg d-flex">
+          <Container className="container d-flex me-auto">
+            <Navbar.Brand href="#home">BCR</Navbar.Brand>
+            <Navbar.Toggle aria-controls="me-auto basic-navbar-nav" />
+            <Navbar.Collapse id="me-auto">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Our Services</Nav.Link>
+                <Nav.Link href="#link">Why Us</Nav.Link>
+                <Nav.Link href="#link">Testimonial</Nav.Link>
+                <Nav.Link href="#link">FAQ</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <div className="container SectionSatu d-flex">
+          <div className="kiri">
+            <div className="Sewa">
+              Sewa dan Rental Mobil Terbaik di kawasan (Lokasimu)
+            </div>
+            <div className="Selamat">
+              Selamat datang di Binar Car Rental. Kami menyediakan mobil
+              kualitas terbaik dengan harga terjangkau. Selalu siap melayani
+              kebutuhanmu untuk sewa mobil selama 24 jam.
+            </div>
+            <div className="Tombol">
+              <Button variant="success">Mulai Sewa Mobil</Button>
+            </div>
+          </div>
+          <div className="Kanan">
+            <img alt="Mercy" src={GambarMobil} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
