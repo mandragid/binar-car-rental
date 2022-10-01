1958972d3a51f968cc3de0c6601582d7e7f03f86
Andre <andreivanp@gmail.com>
2022-10-01 13:57:06 +0700

    add card

---
+++ src/components/WhyUs.js
@@ -0,0 +1,61 @@
+import "./WhyUs.css";
+import React from "react";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+import Jempol from "../img/jempol.png";
+import Harga from "../img/harga.png";
+import Waktu from "../img/waktu.png";
+import Sopir from "../img/sopir.png";
+
+function WhyUs() {
+	return (
+		<div className="container">
+			<div className="WhyUs">
+				<div className="row WhyUs text-start">Why Us?</div>
+				<div className="row Mengapa">Mengapa harus pilih Binar Car Rental?</div>
+			</div>
+			<div className="CardGroup d-flex">
+				<CardGroup className="CardGroup d-flex">
+					<div className="Card">
+						<Card>
+							<Card.Img variant="top" src={Jempol} />
+							<Card.Body>
+								<Card.Title className="JudulCard">Mobil Lengkap</Card.Title>
+								<Card.Text className="CaptionCard">Tersedia banyak pilihan mobil, kondisi masih baru, bersih dan terawat.</Card.Text>
+							</Card.Body>
+						</Card>
+					</div>
+					<div className="Card">
+						<Card>
+							<Card.Img variant="top" src={Harga} />
+							<Card.Body>
+								<Card.Title className="JudulCard">Harga Murah</Card.Title>
+								<Card.Text className="CaptionCard">Harga murah dan bersaing, bisa bandingkan harga kami dengan rental mobil lain.</Card.Text>
+							</Card.Body>
+						</Card>
+					</div>
+					<div className="Card">
+						<Card>
+							<Card.Img variant="top" src={Waktu} />
+							<Card.Body>
+								<Card.Title className="JudulCard">Layanan 24 Jam</Card.Title>
+								<Card.Text className="CaptionCard">Siap melayani kebutuhan Anda selama 24 jam nonstop. Kami juga tersedia di akhir minggu.</Card.Text>
+							</Card.Body>
+						</Card>
+					</div>
+					<div className="Card">
+						<Card>
+							<Card.Img variant="top" src={Sopir} />
+							<Card.Body>
+								<Card.Title className="JudulCard">Sopir Profesional</Card.Title>
+								<Card.Text className="CaptionCard">Sopir yang profesional, berpengalaman, jujur, ramah dan selalu tepat waktu.</Card.Text>
+							</Card.Body>
+						</Card>
+					</div>
+				</CardGroup>
+			</div>
+		</div>
+	);
+}
+
+export default WhyUs;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import "./index.css";
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Services from "./components/Services";
+import WhyUs from "./components/WhyUs";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<NavBar />
 		<Services />
+		<WhyUs />
 	</React.StrictMode>
 );
